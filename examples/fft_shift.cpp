46caf2a2b3fce279b4b0863d6798405087730e27
Harold <pxduharold@gmail.com>
2020-10-10 13:49:41 +0800

    x fft_shift should on full spectrum

--- examples/fft_shift.cpp
+++ examples/fft_shift.cpp
@@ -4,8 +4,10 @@
 
 #include <iostream>
 #include "m_fft_shift.h"
+#include "m_fft.h"
 
-using namespace M_MATH;
+using M_MATH::FFT_SHIFT;
+using M_MATH::FFT2D;
 
 template<typename T>
 void print_v(std::vector<T> const& v) {
@@ -16,18 +18,35 @@ void print_v(std::vector<T> const& v) {
 
 int main() {
     std::vector<std::complex<double>> vc{0, 1, 2, 3, 4, 5, 6};
-    fft_shift_1d(vc.data(), vc.size());
+    FFT_SHIFT::fft_shift_1d(vc.data(), vc.size());
     print_v(vc);
-    ifft_shift_1d(vc.data(), vc.size());
+    FFT_SHIFT::ifft_shift_1d(vc.data(), vc.size());
     print_v(vc);
 
     std::vector<std::complex<double>> vc2{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    fft_shift_2d(vc2.data(), 3, 3);
+    FFT_SHIFT::fft_shift_2d(vc2.data(), 3, 3);
     print_v(vc2);
-    ifft_shift_2d(vc2.data(), 3, 3);
+    FFT_SHIFT::ifft_shift_2d(vc2.data(), 3, 3);
     print_v(vc2);
-    ifft_shift_2d(vc2.data(), 3, 3);
+    FFT_SHIFT::ifft_shift_2d(vc2.data(), 3, 3);
     print_v(vc2);
+
+    std::vector<std::complex<double>> vo(14);
+    FFT_SHIFT::half2full_1d(vc.data(), vo.data(), 14);
+    print_v(vo);
+
+    std::vector<std::complex<double>> vc1{0, 1, 2, 3, 4};
+    std::vector<std::complex<double>> vo2(9);
+    FFT_SHIFT::half2full_1d(vc1.data(), vo2.data(), 9);
+    print_v(vo2);
+
+    std::vector<double> A{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<std::complex<double>> cpx(6);
+    FFT2D::rfft(A.data(), cpx.data(), 3, 3);
+    print_v(cpx);
+    // FIXME: incorrect here, need full spectrum instead of fftw's half spectrum
+    FFT_SHIFT::fft_shift_2d(cpx.data(), 3, 2);
+    print_v(cpx);
 
     return 0;
 }
--- include/m_fft_shift.h
+++ include/m_fft_shift.h
@@ -5,45 +5,55 @@
 #ifndef M_MATH_M_FFT_SHIFT_H
 #define M_MATH_M_FFT_SHIFT_H
 
-// code from: https://stackoverflow.com/questions/5915125/fftshift-ifftshift-c-c-source-code
-
 #include <complex>
 #include <algorithm>
 #include <vector>
 
 namespace M_MATH {
-    // T = std::complex<Real>
-    template <typename T>
-    static inline
-    void fft_shift_1d(T *data, size_t const ydim)
-    {
+    // FFT shift
+    // Note: in-place change
+    class FFT_SHIFT {
+    public:
+        template<typename T>
+        static void fft_shift_1d(T *data, size_t ydim);
+        template<typename T>
+        static void ifft_shift_1d(T *data, size_t ydim);
+
+        template<typename T>
+        static void fft_shift_2d(T *data, size_t xdim, size_t ydim);
+        template<typename T>
+        static void ifft_shift_2d(T *data, size_t xdim, size_t ydim);
+
+        // helper functions: transfer fftw half spectrum to full spectrum
+        // xdim, ydim are original data dims
+        template<typename T>
+        static void half2full_1d(std::complex<T>* fft_data, std::complex<T>* out_data, size_t ydim);
+        template<typename T>
+        static void half2full_2d(std::complex<T>* fft_data, std::complex<T>* out_data, size_t xdim, size_t ydim);
+    };
+
+
+    template<typename T>
+    void FFT_SHIFT::fft_shift_1d(T *data, size_t ydim) {
         auto center = ydim / 2;
         if (ydim % 2 != 0) {
             center++;
         }
-        // odd: 012 34 changes to 34 012
-        std::rotate(data, data + center, data + ydim);
-    }
-
-    template <typename T>
-    static inline
-    void ifft_shift_1d(T *data, size_t const ydim)
-    {
-        auto center = ydim / 2;
-        // odd: 01 234 changes to 234 01
         std::rotate(data, data + center, data + ydim);
     }
 
     template<typename T>
-    static inline
-    void fft_shift_2d(T *data, size_t xdim, size_t ydim)
-    {
+    void FFT_SHIFT::ifft_shift_1d(T *data, size_t ydim) {
+        auto center = ydim / 2;
+        std::rotate(data, data + center, data + ydim);
+    }
+
+    template<typename T>
+    void FFT_SHIFT::fft_shift_2d(T *data, size_t xdim, size_t ydim) {
         size_t xshift = xdim / 2;
         size_t yshift = ydim / 2;
         if ((xdim*ydim) % 2 != 0) {
-            // temp output array
-            std::vector<T> out;
-            out.resize(xdim * ydim);
+            std::vector<T> out(xdim * ydim);
             for (size_t x = 0; x < xdim; x++) {
                 size_t outX = (x + xshift) % xdim;
                 for (size_t y = 0; y < ydim; y++) {
@@ -52,12 +62,9 @@ namespace M_MATH {
                     out[outX + xdim * outY] = data[x + xdim * y];
                 }
             }
-            // copy out back to data
-            copy(out.begin(), out.end(), &data[0]);
+            std::copy(out.begin(), out.end(), &data[0]);
         }
         else {
-            // in and output array are the same,
-            // values are exchanged using std::swap
             for (size_t x = 0; x < xdim; x++) {
                 size_t outX = (x + xshift) % xdim;
                 for (size_t y = 0; y < yshift; y++) {
@@ -69,10 +76,8 @@ namespace M_MATH {
         }
     }
 
-    template<class T>
-    static inline
-    void ifft_shift_2d(T *data, size_t xdim, size_t ydim)
-    {
+    template<typename T>
+    void FFT_SHIFT::ifft_shift_2d(T *data, size_t xdim, size_t ydim) {
         size_t xshift = xdim / 2;
         if (xdim % 2 != 0) {
             xshift++;
@@ -82,9 +87,7 @@ namespace M_MATH {
             yshift++;
         }
         if ((xdim*ydim) % 2 != 0) {
-            // temp output array
-            std::vector<T> out;
-            out.resize(xdim * ydim);
+            std::vector<T> out(xdim * ydim);
             for (size_t x = 0; x < xdim; x++) {
                 size_t outX = (x + xshift) % xdim;
                 for (size_t y = 0; y < ydim; y++) {
@@ -93,12 +96,9 @@ namespace M_MATH {
                     out[outX + xdim * outY] = data[x + xdim * y];
                 }
             }
-            // copy out back to data
-            copy(out.begin(), out.end(), &data[0]);
+            std::copy(out.begin(), out.end(), &data[0]);
         }
         else {
-            // in and output array are the same,
-            // values are exchanged using std::swap
             for (size_t x = 0; x < xdim; x++) {
                 size_t outX = (x + xshift) % xdim;
                 for (size_t y = 0; y < yshift; y++) {
@@ -108,6 +108,21 @@ namespace M_MATH {
                 }
             }
         }
+    }
+
+    template<typename T>
+    void FFT_SHIFT::half2full_1d(std::complex<T>* fft_data, std::complex<T>* out_data, size_t ydim){
+        auto fft_size = ydim % 2 == 0 ? ydim/2 : ydim/2+1;
+        for (auto i = 0; i < fft_size; ++i)
+            out_data[i] = fft_data[i];
+        for (auto i = fft_size; i < ydim; ++i)
+            out_data[i] = std::conj(fft_data[ydim-1 - i]);
+    }
+
+    template<typename T>
+    void FFT_SHIFT::half2full_2d(std::complex<T> *fft_data, std::complex<T> *out_data, size_t xdim, size_t ydim) {
+        // FIXME: not implement yet
+        throw;
     }
 }
 
