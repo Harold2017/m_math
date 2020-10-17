fa1e7a0e27d13012e02fdd28ffbd73ce1bb0c852
Harold <pxduharold@gmail.com>
2020-10-17 10:57:54 +0800

    x simplify histogram

--- include/m_hist_hm.h
+++ include/m_hist_hm.h
@@ -13,12 +13,10 @@ namespace M_MATH {
         T deltaZ = (max - min) / N;
         std::vector<size_t> hist(N, 0);
         for (auto it = begin; it != end; ++it)
-            // [min, min + deltaZ) -> hist[0]
-            if (gfp(*it) < min + deltaZ) ++hist[0];
-                // [max - deltaZ, max] -> hist[N-1]
-            else if (gfp(*it) >= max - deltaZ) ++hist[N-1];
-                // [min + deltaZ : deltaZ : max - deltaZ)
-            else ++hist[int((gfp(*it) - min) / deltaZ)];
+            // [min : deltaZ : max)
+            if (gfp(*it) < max) ++hist[int((gfp(*it) - min) / deltaZ)];
+            // max -> hist[N-1]
+            else ++hist[N-1];
         return hist;
     }
 }
