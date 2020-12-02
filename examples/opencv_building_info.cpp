95adfec96dae3a48999db66186ccfc54c77d1838
Harold <pxduharold@gmail.com>
2020-12-02 11:28:53 +0800

    + print opencv building info

---
+++ examples/opencv_building_info.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Harold on 2020/12/2.
+//
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main()
+{
+    // Get build information
+    std::cout << cv::getBuildInformation();
+}
