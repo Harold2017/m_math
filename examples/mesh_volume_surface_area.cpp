ace1a2fead42427a15a2a92b97c8e1397f1579de
Harold <pxduharold@gmail.com>
2021-02-05 12:40:57 +0800

    + truncated triangular prism volume

--- examples/mesh_volume_surface_area.cpp
+++ examples/mesh_volume_surface_area.cpp
@@ -3,11 +3,13 @@
 //
 
 #include <iostream>
+#include "m_mesh.h"
 #include "m_mesh_volume_surface_area.h"
 
 using namespace M_MATH;
 
 int main() {
+    /*
     // cubic
     std::vector<cv::Point3f> vertices = {{0, 0, 0},
                                          {1, 0, 0},
@@ -33,6 +35,29 @@ int main() {
 
     std::cout << MeshVolume(vertices, triangles) << std::endl;
     std::cout << MeshSurface(vertices, triangles) << std::endl;
+     */
 
+    auto mesh = M_MATH::TriangleMesh::LoadMesh("x.ply");
+    std::cout //<< "volume: " << mesh->GetVolume() << '\n'
+            << "surface area: " << mesh->GetSurfaceArea()
+            << std::endl;
+    mesh->RemoveDuplicatedVertices();
+    mesh->RemoveDuplicatedTriangles();
+    std::vector<cv::Point3d> vert;
+    std::vector<cv::Point3i> tri;
+    vert.reserve(mesh->vertices_.size());
+    tri.reserve(mesh->vertices_.size());
+    for (auto const& v : mesh->vertices_)
+        vert.emplace_back(v.x(), v.y(), v.z());
+    for (auto const& t : mesh->triangles_)
+        tri.emplace_back(t.x(), t.y(), t.z());
+    std::cout << "mesh volume: " << MeshVolume(vert, tri) << std::endl;
+    std::cout << "mesh surface: " << MeshSurface(vert, tri) << std::endl;
+    auto min_v = std::min_element(mesh->vertices_.begin(), mesh->vertices_.end(),
+                                  [](Eigen::Vector3d const& v1, Eigen::Vector3d const& v2){
+        return v1.z() < v2.z();
+    });
+    std::cout << "mesh vertex min: " << "[" << min_v->x() << ", " << min_v->y() << ", " << min_v->z() << "]" << std::endl;
+    std::cout << "mesh volume against plane: " << MeshVolume(vert, tri, {0, 0, min_v->z()}, {0, 0, 1});
     return 0;
 }
--- include/m_mesh_volume_surface_area.h
+++ include/m_mesh_volume_surface_area.h
@@ -41,6 +41,42 @@ namespace M_MATH {
             area += AreaOfTriangle(vertices[t.x], vertices[t.y], vertices[t.z]);
         return area;
     }
+
+    // truncated triangular prism: V = 1/3 * A * (h1 + h2 + h3)
+    template<typename T>
+    inline T TriangularPrismVolume(cv::Point3_<T> const& v1,
+                                   cv::Point3_<T> const& v2,
+                                   cv::Point3_<T> const& v3,
+                                   cv::Point3_<T> const& plane_center,
+                                   cv::Point3_<T> const& plane_normal) {
+        // project vertices onto plane
+        auto v1p = v1 - (v1 - plane_center).dot(plane_normal) * plane_normal / cv::norm(plane_normal);
+        auto v2p = v2 - (v2 - plane_center).dot(plane_normal) * plane_normal / cv::norm(plane_normal);
+        auto v3p = v3 - (v3 - plane_center).dot(plane_normal) * plane_normal / cv::norm(plane_normal);
+
+        auto A = AreaOfTriangle(v1p, v2p, v3p);
+        auto h1 = cv::norm(v1p - v1);
+        auto h2 = cv::norm(v2p - v2);
+        auto h3 = cv::norm(v3p - v3);
+
+        return 1./3. * A * (h1 + h2 + h3);
+    }
+
+    // triangle mesh volume against certain plane (project triangle to plane and compute truncated triangular prism)
+    template<typename T>
+    inline T MeshVolume(std::vector<cv::Point3_<T>> const& vertices,
+                        std::vector<cv::Point3i> const& triangle_v_idx,
+                        cv::Point3_<T> const& plane_center,
+                        cv::Point3_<T> const& plane_normal) {
+        auto volume = T{};
+        for (auto & t : triangle_v_idx)
+            volume += TriangularPrismVolume(vertices[t.x],
+                                            vertices[t.y],
+                                            vertices[t.z],
+                                            plane_center,
+                                            plane_normal);
+        return cv::abs(volume);
+    }
 }
 
 #endif //M_MATH_M_MESH_VOLUME_SURFACE_AREA_H
