3adb44b94101500568ada0300a6ebe301a65193b
Harold2017 <pxduharold@126.com>
2021-03-11 11:27:02 +0800

    x update img_tmpl_match test

--- examples/img_tmpl_match.cpp
+++ examples/img_tmpl_match.cpp
@@ -8,24 +8,42 @@
 
 using namespace M_MATH;
 
+cv::Point pt(0, 0);
+static void onMouse(int event, int x, int y, int, void* params);
+
 int main(int argc, char* argv[]) {
     assert(argc == 3);
-    auto pt = cv::Point{ 300, 50 };
     auto tm = TmplMatching(argv[1], argv[2]);  // srcImg, tmplImg
     tm.Match(cv::TM_CCORR_NORMED);
     tm.DetectEdge();
-    auto pt_m = tm.Locate(pt);
-    std::cout << pt_m << std::endl;
 
-    cv::circle(tm._tmplImg, pt, 1, cv::Scalar(0, 0, 255), cv::FILLED, cv::LINE_8);
+    cv::namedWindow("tmplImg", cv::WINDOW_AUTOSIZE);
     cv::imshow("tmplImg", tm._tmplImg);
 
+    cv::namedWindow("srcImg", cv::WINDOW_AUTOSIZE);
     auto rect = cv::Rect(tm._matchLoc.x, tm._matchLoc.y, tm._tmplImg.cols, tm._tmplImg.rows);
     cv::rectangle(tm._srcImg, rect, cv::Scalar(0, 0, 255));
-    cv::circle(tm._srcImg, pt_m, 1, cv::Scalar(0, 0, 255), cv::FILLED, cv::LINE_8);
     cv::imshow("srcImg", tm._srcImg);
 
+    cv::setMouseCallback("tmplImg", onMouse, &tm);
+
     cv::waitKey();
+    cv::destroyAllWindows();
     
     return 0;
+}
+
+static void onMouse(int event, int x, int y, int, void* params)
+{
+    if (event != cv::EVENT_LBUTTONDOWN)
+        return;
+
+    auto* tm = (TmplMatching*)params;
+    cv::Mat* img = &tm->_tmplImg;
+    pt = cv::Point(x, y);
+    cv::circle(*img, pt, 1, cv::Scalar(0, 0, 255), cv::FILLED, cv::LINE_8);
+    auto pt_m = tm->Locate(pt);
+    cv::circle(tm->_srcImg, pt_m, 1, cv::Scalar(0, 0, 255), cv::FILLED, cv::LINE_8);
+    cv::imshow("tmplImg", *img);
+    cv::imshow("srcImg", tm->_srcImg);
 }
