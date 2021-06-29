5fba136f54cc08e4b3077b65ecdf92c307a73e9a
Harold2017 <pxduharold@126.com>
2021-06-29 12:46:53 +0800

    x fix autoc_length

--- examples/autoc_length.cpp
+++ examples/autoc_length.cpp
@@ -9,7 +9,7 @@
 #include "m_opencv_utils.h"
 
 int main() {
-    cv::Mat I = imread( cv::samples::findFile( "lena.jpg" ), cv::IMREAD_GRAYSCALE);
+    cv::Mat I = imread( cv::samples::findFile( "test.png" ), cv::IMREAD_GRAYSCALE);
     if( I.empty()){
         std::cout << "Error opening image" << std::endl;
         return EXIT_FAILURE;
@@ -22,17 +22,17 @@ int main() {
     cv::imshow("auto_corr", auto_corr);
     //std::cout << auto_corr << std::endl;
 
-    // use threshold (1 - s) to filter the bright ellipse area
-    cv::threshold(auto_corr, auto_corr, 0.8, 1, cv::THRESH_BINARY);
+    // use threshold s to filter the bright ellipse area
+    cv::threshold(auto_corr, auto_corr, 0.5, 1, cv::THRESH_BINARY);
     cv::imshow("corr_thr", auto_corr);
 
     // find contour of this edge
     auto auto_corr_8U = M_MATH::To8U(auto_corr);
     std::vector<std::vector<cv::Point>> contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(auto_corr_8U, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+    cv::findContours(auto_corr_8U, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    std::cout << "contours size: " << contours.size() << std::endl;
     cv::Mat drawing = cv::Mat::zeros(auto_corr_8U.size(), CV_8UC1);
-    cv::drawContours(drawing, contours, -1, 255, 1, cv::LINE_8, hierarchy, 0);
+    cv::drawContours(drawing, contours, -1, 255, 1);
     cv::imshow("Contours", drawing);
 
     // no contour
@@ -42,22 +42,27 @@ int main() {
                   << std::endl;
         return 0;
     }
-    // only center
-    if (contours[contours.size() - 1].size() < 2) {
-        std::cout << "auto-correlation length: " << 0 << '\n'
-                  << "texture aspect ratio: " << 1
-                  << std::endl;
-        return 0;
-    }
 
     // center is always at image center
     cv::Point center{I.cols/2, I.rows/2};
+
+    // find contour which surrond center
+    int target_contour_idx = -1;
+    for (auto i = 0; i < contours.size(); i++)
+        if (cv::pointPolygonTest(contours[i], center, false) >= 0)
+            target_contour_idx = i;
+    if (target_contour_idx == -1) {
+        std::cout << "auto-correlation length: " << 0 << '\n'
+                  << "texture aspect ratio: " << 0
+                  << std::endl;
+        return 0;
+    }
 
     // find shortest radius and longest radius
     double max = std::numeric_limits<double>::min();
     double min = std::numeric_limits<double>::max();
     cv::Point max_r, min_r;
-    for (auto &pt : contours[contours.size() - 1]) {
+    for (auto &pt : contours[target_contour_idx]) {
         if (cv::norm(pt - center) > max) {
             max = cv::norm(pt - center);
             max_r = pt;
@@ -72,6 +77,8 @@ int main() {
               << std::endl;
 
     // draw max, min radius
+    drawing = 0;
+    cv::drawContours(drawing, contours, target_contour_idx, 255, 1);
     cv::line(drawing, center, max_r, 255);
     cv::line(drawing, center, min_r, 255);
     cv::imshow("max_min", drawing);
