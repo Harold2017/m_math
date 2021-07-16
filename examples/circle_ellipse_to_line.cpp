ea2cd45ecee5b8f19cfcdf26f3b27a439e6a7d5e
Harold <pxduharold@gmail.com>
2021-07-16 17:36:24 +0800

    + arc to line

--- examples/circle_ellipse_to_line.cpp
+++ examples/circle_ellipse_to_line.cpp
@@ -25,11 +25,39 @@ std::vector<cv::Point_<T>> EllipseToLine(cv::Mat const& I, cv::Point_<T> const& 
     N = N > 360 ? 360 : N;  // 1 degree as min theta
     std::vector<cv::Point2d> points;
     cv::ellipse2Poly(cv::Point2d(center.x, center.y), cv::Size2d(radius.x, radius.y), 0, 0, 360, int(360 / N), points);
-    std::vector<cv::Point_<T>>  pts(N-1);
+    std::vector<cv::Point_<T>>  pts(points.size()-1);
     pts[0].x = 0;
     pts[0].y = I.at<T>(cvRound(points[0].x), cvRound(points[0].y));
     T x = 0;
-    for (auto i = 1; i < N-1; i++) {
+    for (auto i = 1; i < points.size()-1; i++) {
+        x += static_cast<T>(cv::norm(points[i] - points[i-1]));
+        pts[i].x = x;
+        pts[i].y = I.at<T>(cvRound(points[i].x), cvRound(points[i].y));
+    }
+    return pts;
+}
+
+template<typename T>
+std::vector<cv::Point_<T>> ArcToLine(cv::Mat const& I,
+                                     cv::Point_<T> const& center,
+                                     cv::Point_<T> const& start_point,
+                                     cv::Point_<T> const& end_point,
+                                     size_t N = 360) {
+    N = N > 360 ? 360 : N;  // 1 degree as min theta
+    auto radius = cv::norm(start_point - center);
+    if (1. / CV_PI * radius > cv::norm(start_point - end_point))
+        throw "start and end point is too close (theta < 1 degrees)";
+    auto l1 = start_point - center;
+    auto l2 = end_point - center;
+    auto start_angle = std::atan2(l1.y, l1.x) / CV_2PI * 360;
+    auto end_angle = std::atan2(l2.y, l2.x) / CV_2PI * 360;
+    std::vector<cv::Point2d> points;
+    cv::ellipse2Poly(cv::Point2d(center.x, center.y), cv::Size2d(radius, radius), 0, start_angle, end_angle, int(360 / N), points);
+    std::vector<cv::Point_<T>>  pts(points.size()-1);
+    pts[0].x = 0;
+    pts[0].y = I.at<T>(cvRound(points[0].x), cvRound(points[0].y));
+    T x = 0;
+    for (auto i = 1; i < points.size()-1; i++) {
         x += static_cast<T>(cv::norm(points[i] - points[i-1]));
         pts[i].x = x;
         pts[i].y = I.at<T>(cvRound(points[i].x), cvRound(points[i].y));
@@ -58,6 +86,9 @@ int main(int argc, char* argv[]) {
 
     auto ellipse_line_pts = EllipseToLine(I, cv::Point2f(256, 256), cv::Point2f(30, 50));
     print_pt_vec(ellipse_line_pts, "ellipse to line pts");
+
+    auto arc_line_pts = ArcToLine(I, cv::Point2f(256, 256), cv::Point2f(300, 300), cv::Point2f(212, 300));
+    print_pt_vec(arc_line_pts, "arc to line pts");
 
     return 0;
 }
